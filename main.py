5ce1255acc6352d2aa3f3a24dbacfcfb56567ab2
unknown <fariedfarizi24@gmail.com>
2023-12-15 15:40:16 +0700

    harusnya uda

--- main.py
+++ main.py
@@ -1,72 +1,80 @@
+import os
 from manajemen_kontak import RedBlackTree
+from tabulate import tabulate
 
 if __name__ == "__main__":
     rb_tree = RedBlackTree()
-
     while True:
+        os.system("cls")
+        rb_tree.display_contacts(descending=False)
         print("\nMenu:")
         print("1. Tambah Kontak")
         print("2. Cari Kontak")
         print("3. Tampilkan Semua Kontak")
         print("4. Hapus Kontak")
         print("5. Edit Kontak")
-        print("6. Simpan Data ke File")
-        print("0. Keluar")
+        print("6. Simpan dan Keluar")
 
         choice = input("Pilih menu: ")
 
         if choice == "1":
+            os.system("cls")
             rb_tree.display_contacts(descending=False)
-            nomor = input("Nomor Kontak: ")
             nama = input("Nama Kontak: ")
-            rb_tree.insert(nomor, nama)
+            nomor = input("Nomor Kontak: ")
+            rb_tree.insert(nama, nomor)
+
         elif choice == "2":
-            print("Mencari kontak berdasarkan")
-            print("[1] Nomor")
-            print("[2] Nama")
-            pili = input("--> ")
-            if pili == "1":
-                nomor = input("Masukkan nomor untuk mencari kontak: ")
-                rb_tree.search_and_display(nomor, kontak = True)
-            elif pili == "2":
-                nama = input("Masukkan nama untuk mencari kontak: ")
-                rb_tree.search_and_display(nama, kontak = False)
+            os.system("cls")
+            print("Mencari kontak... ")
+            nama = input("--> ")
+            result_node = rb_tree.search_nama(nama)
+            if result_node != rb_tree.NIL:
+                data = [[node.nama, node.nomor] for node in result_node]
+                print(tabulate(data, headers=["Nama", "Nomor"], tablefmt='fancy_grid'))
+            else:
+                print(f"\nNilai '{nama}' tidak ditemukan dalam data.")
+
+            input("\nTekan enter untuk kembali ke menu")
                 
         elif choice == "3":
+            os.system("cls")
             print("Tampilkan Berdasarkan")
             print("1. Ascending")
-            print("1. Descending")
+            print("2. Descending")
             pilih = input(">> ")
             if pilih == "1":
                 rb_tree.display_contacts(descending=False)
+                input("\nTekan enter untuk kembali ke menu")
             elif pilih == "2":
                 rb_tree.display_contacts(descending=True)
+                input("\nTekan enter untuk kembali ke menu")
+
         elif choice == "4":
+            os.system("cls")
             rb_tree.display_contacts(descending=False)
-            nomor = input("Masukkan nomor untuk menghapus kontak: ")
-            rb_tree.delete(nomor)
+            nama = input("Masukkan nama untuk menghapus kontak: ")
+            rb_tree.delete(nama)
+
         elif choice == "5":
             rb_tree.display_contacts(descending=False)
             nomor = input("Masukkan nomor untuk mengedit kontak: ")
-            result = rb_tree.search(nomor)
-            if result != rb_tree.NIL:
-                new_nama = input("Masukkan nama baru: ")
-                result.value = new_nama
-                print("Kontak diedit.")
-            else:
-                print("Kontak tidak ditemukan.")
+            new_nomor = input("Masukkan nomor baru: ")
+            new_nama = input("Masukkan nama baru: ")
+            rb_tree.edit_contact(nomor, new_nomor, new_nama)
+
         elif choice == "6":
             konfirmasi = input("Apakah anda yakin ingin menyimpan perubahan ? (y/n)")
             if konfirmasi == "y":
                 filename = "kontak.csv"
                 rb_tree.save_to_file(filename)
-                print("Data disimpan ke file.")
+                print("Data berhasil disimpan ke file.")
+                break
             elif konfirmasi == "n":
                 print("Kembali ke menu")
                 continue
             else:
                 print("Invalid Input!")
-        elif choice == "0":
-            break
+
         else:
             print("Pilihan tidak valid. Silakan coba lagi.")
--- manajemen_kontak.py
+++ manajemen_kontak.py
@@ -2,9 +2,9 @@ from tabulate import tabulate
 import csv
 
 class RedBlackTreeNode:
-    def __init__(self, key, value, color, left=None, right=None, parent=None):
-        self.key = key  # Nomor telepon sebagai kunci
-        self.value = value
+    def __init__(self, nama, nomor, color, left=None, right=None, parent=None):
+        self.nama = nama
+        self.nomor = nomor
         self.color = color
         self.left = left
         self.right = right
@@ -16,15 +16,15 @@ class RedBlackTree:
         self.root = self.NIL
         self.load_from_file(data_file)
 
-    def insert(self, key, value):
-        new_node = RedBlackTreeNode(key, value, "RED", self.NIL, self.NIL, self.NIL)
+    def insert(self, nama, nomor):
+        new_node = RedBlackTreeNode(nama, nomor, "RED", self.NIL, self.NIL, self.NIL)
         self._insert_node(self.root, new_node)
         self._fix_insert(new_node)
 
     def _insert_node(self, root, new_node):
         if root == self.NIL:
             self.root = new_node
-        elif new_node.key < root.key:
+        elif new_node.nama < root.nama:
             if root.left == self.NIL:
                 root.left = new_node
                 new_node.parent = root
@@ -39,34 +39,43 @@ class RedBlackTree:
 
     def _fix_insert(self, node):
         while node.parent.color == "RED":
+            # Kasus 1: Orang tua adalah kiri dari kakek
             if node.parent == node.parent.parent.left:
                 y = node.parent.parent.right
+                # Kasus 1a: Paman (y) adalah merah
                 if y.color == "RED":
                     node.parent.color = "BLACK"
                     y.color = "BLACK"
                     node.parent.parent.color = "RED"
                     node = node.parent.parent
                 else:
+                    # Kasus 1b: Node adalah anak kanan
                     if node == node.parent.right:
                         node = node.parent
                         self._left_rotate(node)
+                    # Kasus 1c: Node adalah anak kiri
                     node.parent.color = "BLACK"
                     node.parent.parent.color = "RED"
                     self._right_rotate(node.parent.parent)
             else:
+                # Kasus 2: Orang tua adalah kanan dari kakek (simetris dengan Kasus 1)
                 y = node.parent.parent.left
+                # Kasus 2a: Paman (y) adalah merah
                 if y.color == "RED":
                     node.parent.color = "BLACK"
                     y.color = "BLACK"
                     node.parent.parent.color = "RED"
                     node = node.parent.parent
                 else:
+                    # Kasus 2b: Node adalah anak kiri
                     if node == node.parent.left:
                         node = node.parent
                         self._right_rotate(node)
+                    # Kasus 2c: Node adalah anak kanan
                     node.parent.color = "BLACK"
                     node.parent.parent.color = "RED"
                     self._left_rotate(node.parent.parent)
+        # Kasus 3: Akar selalu hitam
         self.root.color = "BLACK"
 
     def _left_rotate(self, x):
@@ -99,35 +108,32 @@ class RedBlackTree:
         x.right = y
         y.parent = x
 
-    def search(self, key):
-        return self._search(self.root, key, nomor)
+    def search_nama(self, nama):
+        result_list = []
+        self._search_helper(self.root, nama, result_list)
+        return result_list
 
-    def _search(self, node, key, kontak):
-        if kontak:
-            if node == self.NIL or key == node.key:
-                return node
-            if key < node.key:
-                return self._search(node.left, key, kontak)
-            return self._search(node.right, key, kontak)
-        else:
-            if node == self.NIL or key.lower() == node.value.lower():
-                return node
-            left_result = self._search(node.left, key, kontak)
-            right_result = self._search(node.right, key, kontak)
-            return left_result if left_result != self.NIL else right_result
+    def _search_helper(self, node, nama, result_list):
+        if node != self.NIL:
+            self._search_helper(node.left, nama, result_list)
+            if nama.lower() in node.nama.lower():
+                result_list.append(node)
+            self._search_helper(node.right, nama, result_list)
 
-    def search_and_display(self, key, kontak):
-        result = self._search(self.root, key, kontak)
-        if result != self.NIL:
-            data = self._inorder_traversal(result)
-            print(tabulate(data, headers=["Nomor", "Nama"], tablefmt="fancy_grid"))
-        else:
-            print("Kontak tidak ditemukan.")
+    def search(self, nama):
+        return self._search(self.root, nama)
+
+    def _search(self, node, nama):
+        if node == self.NIL or nama == node.nama:
+            return node
+        if nama < node.nama:
+            return self._search(node.left, nama)
+        return self._search(node.right, nama)
 
     def _inorder_traversal(self, node, descending=False):
         if node != self.NIL:
             left_data = self._inorder_traversal(node.left, descending)
-            current_data = [[node.key, node.value]]
+            current_data = [[node.nama, node.nomor]]
             right_data = self._inorder_traversal(node.right, descending)
 
             if descending:
@@ -140,14 +146,16 @@ class RedBlackTree:
     def display_contacts(self, descending=False):
         data = self._inorder_traversal(self.root, descending)
         if data:
-            print(tabulate(data, headers=["Nomor", "Nama"], tablefmt="grid"))
+            print(tabulate(data, headers=["Nama", "Nomor"], tablefmt="fancy_grid"))
         else:
             print("Tidak ada data kontak.")
 
-    def delete(self, key):
-        node = self.search(key)
+    def delete(self, nama):
+        node = self.search(nama)
         if node != self.NIL:
             self._delete_node(node)
+        else:
+            print(f"Kontak dengan nama {nama} tidak ditemukan.")
 
     def _delete_node(self, node):
         y = node
@@ -192,21 +200,25 @@ class RedBlackTree:
     def _fix_delete(self, x):
         while x != self.root and x.color == "BLACK":
             if x == x.parent.left:
+                # Kasus 1: Sibling (w) adalah MERAH
                 w = x.parent.right
                 if w.color == "RED":
                     w.color = "BLACK"
                     x.parent.color = "RED"
                     self._left_rotate(x.parent)
                     w = x.parent.right
+                # Kasus 2: Kedua anak dari sibling adalah HITAM
                 if w.left.color == "BLACK" and w.right.color == "BLACK":
                     w.color = "RED"
                     x = x.parent
                 else:
+                    # Kasus 3: Anak kanan dari sibling adalah HITAM
                     if w.right.color == "BLACK":
                         w.left.color = "BLACK"
                         w.color = "RED"
                         self._right_rotate(w)
                         w = x.parent.right
+                    # Kasus 4: Sibling dan anak-anaknya adalah HITAM
                     w.color = x.parent.color
                     x.parent.color = "BLACK"
                     w.right.color = "BLACK"
@@ -235,23 +247,33 @@ class RedBlackTree:
                     x = self.root
         x.color = "BLACK"
 
+    def edit_contact(self, nama, new_nomor, new_nama):
+        node_to_edit = self.search(nama)
+
+        if node_to_edit != self.NIL:
+            node_to_edit.nomor = new_nomor
+            node_to_edit.nama = new_nama
+            print(f"Kontak dengan nama {nama} telah diedit.")
+        else:
+            print(f"Kontak dengan nama {nama} tidak ditemukan.")
+
     def save_to_file(self, filename="kontak.csv"):
         data = self._inorder_traversal(self.root)
         with open(filename, 'w', newline='') as file:
             csv_writer = csv.writer(file)
-            csv_writer.writerow(["Nomor", "Nama"])  # Menulis header
+            csv_writer.writerow(["Nama", "Nomor"])
             csv_writer.writerows(data)
 
     def load_from_file(self, filename="kontak.csv"):
         try:
             with open(filename, 'r') as file:
                 csv_reader = csv.reader(file)
-                next(csv_reader)  # Mengabaikan baris header
+                next(csv_reader)  
                 data = [row for row in csv_reader]
 
             self.root = self.NIL
             for entry in data:
-                key, value = entry
-                self.insert(key, value)
+                nama, value = entry
+                self.insert(nama, value)
         except FileNotFoundError:
             print("File tidak ditemukan. Menggunakan data default.")
